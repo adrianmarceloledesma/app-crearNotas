34ac7593d4ac8a409297af6a30357a1da0939a0d
jOh <j0h@example.com>
2020-05-07 18:49:24 -0300

    primer commit

---
+++ src/components/title.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ function Title() {
+    return (
+        <div>
+            <h1 className="title"> Escribir Notas <span aria-label="emoji" role="img">🔥</span> </h1>
+        </div>
+    )
+}
+export default Title
