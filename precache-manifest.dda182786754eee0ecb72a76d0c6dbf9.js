946b557c41ddcabeacbd4f12a085c9663295a1d4
jOh <j0h@example.com>
2020-05-07 19:23:37 -0300

    Updates

---
+++ precache-manifest.dda182786754eee0ecb72a76d0c6dbf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d14d570ab267adba32e88addfb1d22a7",
+    "url": "/app-crearNotas/index.html"
+  },
+  {
+    "revision": "bb9f63061e37c22d0385",
+    "url": "/app-crearNotas/static/css/main.e2f6f899.chunk.css"
+  },
+  {
+    "revision": "1cde54dc3988b5b27649",
+    "url": "/app-crearNotas/static/js/2.a8ccf602.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app-crearNotas/static/js/2.a8ccf602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb9f63061e37c22d0385",
+    "url": "/app-crearNotas/static/js/main.2e305443.chunk.js"
+  },
+  {
+    "revision": "7e0be79df90759071cd8",
+    "url": "/app-crearNotas/static/js/runtime-main.1064d400.js"
+  }
+]);
